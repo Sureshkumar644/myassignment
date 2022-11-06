2a3ae0d1fca5829993f887b45ec86a2fb3d81288
Sureshkumar644 <117565013+Sureshkumar644@users.noreply.github.com>
2022-11-06 13:57:51 +0530

    Add files via upload
    day1

---
+++ day1/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		Mobile phone = new Mobile();
+		
+		phone.makeCall();
+		phone.sendMsg();
+	}
+
+	public void makeCall() {
+		// TODO Auto-generated method stub
+	 System.out.println("Test1");	
+	}
+	
+	public void sendMsg() {
+		// TODO Auto-generated method stub
+		System.out.println("Test2");
+	}
+
+	
+}
+
